5d441113245f310a65ccf2e9072689448d5d89f1
Tim Kundakchan <kundakchan@yandex.ru>
2020-03-05 12:23:08 +0300

    Начало проекта

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,4 @@
+<template>
+  <dir>
+  </dir>
+</template>
