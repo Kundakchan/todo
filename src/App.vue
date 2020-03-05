c7e72080f9f8e3bd5975bb2fead33f4bb5c067f0
Tim Kundakchan <kundakchan@yandex.ru>
2020-03-05 14:01:41 +0300

    Реализован header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
+    <header class="app-header">
+      <TheHeader/>
+    </header>
+    <main class="app-main">
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <script>
+import TheHeader from '@/components/TheHeader'
 export default {
+  components: {
+    TheHeader
+  },
   data () {
     return {}
   }
 }
 </script>
+
+<style lang="scss">
+.app-header, .app-main {
+  margin: 0 auto 0;
+  max-width: 1230px;
+  padding: 0 .9375rem;
+  width: 100%;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header w_max flex flex_between flex_center_vertical">
+    <a href="#" @click="$_header_pushHome" class="login">
+      <p>{{ login }}</p>
+    </a>
+    <button  @click="$_header_pushAdd" class="btn btn_create icon-create"></button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      login: 'todo'
+    }
+  },
+  methods: {
+    $_header_pushHome () {
+      this.$router.push('Home')
+    },
+    $_header_pushAdd () {
+      this.$router.push('Edit')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.header {
+  padding: 1rem 0;
+}
+.login {
+  text-transform: capitalize;
+  font-size: 3rem;
+  p:first-letter {
+    color: orange;
+  }
+  p {
+    color: black;
+  }
+}
+.btn_create {
+  border-radius: 100%;
+  font-size: 2rem;
+  color: rgb(105, 105, 253);
+  background-color: rgb(243, 242, 242);
+  &:active {
+    background-color: darken(rgb(243, 242, 242), 5%);
+  }
+}
+</style>
